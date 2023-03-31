037a5458d9a8fe669089e0bb4f7d3b694ce701e1
remgray <v.greygrayrem@gmail.com>
2023-03-31 18:38:52 +0300

    add to favorite event

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,46 @@ import SongsPlayer from '@/components/SongsPlayer.vue'
 
 const songs = ref<Song[]>([])
 
+const playingSongs: Song[] = [
+  {
+    id: '1',
+    title: 'So What',
+    artist: 'Miles Davis',
+    year: 1959
+  },
+  {
+    id: '2',
+    title: 'Smells Like Teen Spirit',
+    artist: 'Nirvana',
+    year: 1991
+  },
+  {
+    id: '3',
+    title: 'What a Wonderful World',
+    artist: 'Louis  Armstrong',
+    year: 1967
+  },
+  {
+    id: '4',
+    title: 'The Look',
+    artist: 'Roxette',
+    year: 1988
+  }
+]
+
 onMounted(() => {
   getSongs(songs)
 })
+
+const addToFavorite = (songId: string) => {
+  console.log(songId)
+}
 </script>
 
 <template>
   <div class="app">
     <main>
-      <songs-player />
+      <songs-player @add-to-favorite="addToFavorite" :songs="playingSongs" />
       <songs-list :songs="songs" />
     </main>
   </div>
@@ -33,5 +64,9 @@ main {
   display: flex;
   flex-direction: column;
   gap: 20px;
+  max-width: 300px;
+}
+main > * {
+  width: 100%;
 }
 </style>
--- src/components/SongsPlayer.vue
+++ src/components/SongsPlayer.vue
@@ -2,12 +2,31 @@
   <v-card title="Player" class="Player">
     <v-card-text>
       <v-icon icon="mdi-play"></v-icon>
-      Song title
+      {{ currentSong.id }}
+      {{ currentSong.title }}
+      {{ currentSong.year }}
     </v-card-text>
     <v-card-actions>
-      <v-btn>Add to Favorites</v-btn>
+      <v-btn @click="emits('add-to-favorite', currentSong.id)">Add to Favorites</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Song } from '@/types/songs'
+import { ref } from 'vue'
+
+const emits = defineEmits<{
+  (e: 'add-to-favorite', id: string): void
+}>()
+
+const props = defineProps<{
+  songs: Song[]
+}>()
+
+const getRandomSong = () => {
+  return Math.floor(Math.random() * props.songs.length)
+}
+
+const currentSong = ref(props.songs[getRandomSong()])
+</script>
